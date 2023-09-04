b526ce18cb7d9682e2c3eee404a391d6165e4f2c
pcaselas <pablo.caselas@tramitapp.com>
2023-09-04 14:33:52 +0200

    openPdfUri

--- www/AndroidNativePdfViewer.js
+++ www/AndroidNativePdfViewer.js
@@ -3,3 +3,6 @@ var exec = require('cordova/exec');
 exports.openPdfUrl = function(url, header,options,success, error) {
     exec(success, error, "AndroidNativePdfViewer", "openPdfUrl", [url,header,options]);
 };
+exports.openPdfUri = function(uri, options,success, error) {
+    exec(success, error, "AndroidNativePdfViewer", "openPdfUri", [uri,options]);
+};
